ccdbc4f91fc175708b16d15c2fc90969de8334f0
rdeev08 <rochelle.velasquez@hotmail.com>
2025-09-15 21:35:17 -0400

    corrected INPUTS for the env variables

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -5,9 +5,9 @@ from botocore.config import Config
 
 
 def run():
-    bucket = os.environ['INPUT_BUCKET']
-    bucket_region = os.environ['INPUT_BUCKET-REGION']
-    dist_folder = os.environ['INPUT_DIST-FOLDER']
+    bucket = os.environ['INPUT_S3-BUCKET']
+    bucket_region = os.environ['INPUT_AWS-REGION']
+    dist_folder = os.environ['INPUT_SOURCE-DIR']
 
     configuration = Config(region_name=bucket_region)
 
